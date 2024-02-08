0c515cc421b1ea5ff94b48c1ed6859f1a9513206
ErrorRExorY <156946304+ErrorRExorY@users.noreply.github.com>
2024-02-08 23:33:55 +0100

    Update sv_utils.lua

--- server/sv_utils.lua
+++ server/sv_utils.lua
@@ -23,7 +23,7 @@ end
 VersionCheck = function(repository)
     local resource = GetInvokingResource() or GetCurrentResourceName()
 
-    local currentVersion = 'v1.1.55'
+    local currentVersion = 'v1.1.56'
 
     if currentVersion then
         currentVersion = currentVersion:match('%d+%.%d+%.%d+')
