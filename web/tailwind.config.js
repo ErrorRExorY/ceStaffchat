6d1d0cda7910adfb10598c0175f4a6b97b1ab1c2
CaptainExorY <pascal.frerks@gmail.com>
2024-02-08 21:02:54 +0100

    test1

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mantine-primary": "#FF1744",
+        "mantine-secondary": "#2979FF",
+        "mantine-accent": "#FF9100",
+        "mantine-dark": "#202020",
+        "mantine-light": "#F5F5F5",
+      },
+    },
   },
   plugins: [],
 };
