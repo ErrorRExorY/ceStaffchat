7f51ccd0902f7fcc2f2a5b869aebf2f4724cc7bf
CaptainExorY <pascal.frerks@gmail.com>
2024-02-08 20:42:48 +0100

    revert

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,21 +2,8 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        red: {
-          100: '#ffeeee',
-          200: '#ffdddd',
-          300: '#ffcccc',
-          400: '#ffaaaa',
-          500: '#ff8888',
-          600: '#ff6666',
-          700: '#ff4444',
-          800: '#ff2222',
-          900: '#ff0000',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
