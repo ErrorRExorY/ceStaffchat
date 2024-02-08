12af197acb9bdcc88957a72323430c25b0834bbc
ErroRExorY <exoryspielt@gmail.com>
2024-02-08 23:09:19 +0100

    testing

--- server/sv_utils.lua
+++ server/sv_utils.lua
@@ -23,7 +23,7 @@ end
 VersionCheck = function(repository)
     local resource = GetInvokingResource() or GetCurrentResourceName()
 
-    local currentVersion = 'v1.1.53'
+    local currentVersion = 'v1.1.55'
 
     if currentVersion then
         currentVersion = currentVersion:match('%d+%.%d+%.%d+')
