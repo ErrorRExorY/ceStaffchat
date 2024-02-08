84f5935ea6c75ac978d9ccd04bc9fc5795ffc8cd
CaptainExorY <pascal.frerks@gmail.com>
2024-02-08 21:08:27 +0100

    revert

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "mantine-primary": "#FF1744",
-        "mantine-secondary": "#2979FF",
-        "mantine-accent": "#FF9100",
-        "mantine-dark": "#202020",
-        "mantine-light": "#F5F5F5",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
