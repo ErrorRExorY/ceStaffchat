ec5ae89ad72f2b3c5d701a1345258e3c5a9e108e
CaptainExorY <pascal.frerks@gmail.com>
2024-02-08 20:40:46 +0100

    Test

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          100: '#ffeeee',
+          200: '#ffdddd',
+          300: '#ffcccc',
+          400: '#ffaaaa',
+          500: '#ff8888',
+          600: '#ff6666',
+          700: '#ff4444',
+          800: '#ff2222',
+          900: '#ff0000',
+        },
+      },
+    },
   },
   plugins: [],
 };
-
